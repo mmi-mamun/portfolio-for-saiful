f45688442ada0ecd8c4870664c49e8ff09100b46
mmi-mamun <muzahidulislam735@gmail.com>
2023-03-13 00:19:56 +0600

    Home navigation added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,6 +11,7 @@ const Navbar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52  font-bold">
+                        <li><Link to='/home'>Home</Link></li>
                         <li><Link to='/about'>About Me</Link></li>
                         <li><Link to='/education'>Education</Link></li>
                         <li><Link to='/skills'>Skills</Link></li>
@@ -22,8 +23,9 @@ const Navbar = () => {
                 </div>
                 <Link to='/home' className="btn btn-ghost normal-case text-xl"><img className='w-48' src={logo} alt="" /></Link>
             </div>
-            <div className="navbar-end hidden lg:flex">
+            <div className=" hidden lg:flex">
                 <ul className="menu menu-horizontal p-0 font-bold uppercase">
+                    <li><Link to='/home'>Home</Link></li>
                     <li><Link to='/about'>About Me</Link></li>
                     <li><Link to='/education'>Education</Link></li>
                     <li><Link to='/skills'>Skills</Link></li>
